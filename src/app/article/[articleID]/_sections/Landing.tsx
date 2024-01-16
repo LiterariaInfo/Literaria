1c1e567860c29549c6393f80b11256fe4331d3b5
Karo8870 <cazacuchristian@gmail.com>
2024-01-16 19:33:39 +0200

    Added landing page for directories

--- src/app/article/[articleID]/_sections/Landing.tsx
+++ src/app/article/[articleID]/_sections/Landing.tsx
@@ -2,6 +2,8 @@ import GiantArticleCard from '@/components/cards/GiantArticleCard';
 import SpotlightCard from '@/components/cards/SpotlightCard';
 import NextSectionCard from '@/components/cards/NextSectionCard';
 import articleCountFormatter from '@/lib/formatters/articleCountFormatter';
+import SpotlightCardMobile from '@/components/cards/SpotlightCardMobile';
+import NextSectionCardMobile from '@/components/cards/NextSectionCardMobile';
 
 export default () => {
 	const article = {
@@ -18,13 +20,25 @@ export default () => {
 	const articleTitle = 'Perioada pașoptistă';
 
 	return (
-		<section className='section pt-20 pb-10 px-8 mobile:px-4 gap-12 tablet:gap-7 mobile:flex-col'>
-			<div className='flex flex-col grow laptop:pl-5'>
+		<section className='section pt-20 pb-10 px-8 mobile:px-4 gap-12 tablet:gap-7 mobile:gap-3 mobile:flex-col'>
+			<div className='flex flex-col grow'>
 				<h1 className='text-left mb-8 main-title'>{articleTitle}</h1>
 				<GiantArticleCard article={article} />
 			</div>
-			<div className='flex flex-col gap-10 tablet:gap-6 max-w-[30%]'>
-				<SpotlightCard className='tablet:grow' article={article} />
+			<div className='flex flex-col gap-10 tablet:gap-6 mobile:gap-3 max-w-[30%] mobile:max-w-none mobile:flex-row'>
+				<SpotlightCardMobile
+					className='hidden mobile:flex w-1/2'
+					article={article}
+				/>
+				<SpotlightCard
+					className='tablet:grow mobile:hidden'
+					article={article}
+				/>
+				<NextSectionCardMobile
+					className='hidden mobile:flex w-1/2'
+					text={articleCountFormatter(articleCount)}
+					image={article.image}
+				/>
 				<NextSectionCard
 					className='tablet:hidden'
 					text={articleCountFormatter(articleCount)}
--- src/components/cards/NextSectionCardMobile.tsx
+++ src/components/cards/NextSectionCardMobile.tsx
@@ -2,7 +2,7 @@
 
 import NextItemButton from '@/components/buttons/NextItemButton';
 
-const NextSectionCard = ({
+export default ({
 	text,
 	image,
 	className = ''
@@ -14,23 +14,21 @@ const NextSectionCard = ({
 	const handleClick = () => {};
 
 	return (
-		<div className={`relative w-full h-0 grow ${className}`}>
+		<div className={`relative grow ${className}`}>
 			<img
 				src={image}
-				className='object-cover w-full h-full rounded-[3rem] tablet:rounded-[2rem]'
+				className='object-cover w-full h-full rounded-[2rem]'
 				alt='Next section'
 			/>
-			<div className='absolute w-full h-full flex gap-8 items-center justify-center flex-col backdrop-blur-[20px] backdrop-brightness-50 rounded-[3rem] tablet:rounded-[2rem] left-0 top-0 p-4'>
+			<div className='absolute w-full h-full flex gap-8 items-center justify-center flex-col backdrop-blur-[16px] backdrop-brightness-50 rounded-[2rem] left-0 top-0 p-4'>
 				<label className='small-title text-white left-0 top-0 text-center'>
 					{text}
 				</label>
 				<NextItemButton
-					className='rotate-90 tablet:hidden mobile:flex'
+					className='rotate-90 flex'
 					onClick={handleClick}
 				/>
 			</div>
 		</div>
 	);
 };
-
-export default NextSectionCard;
