ec0c3352eb0543737f1f0abf70097d50e9ca1c1e
Karo8870 <cazacuchristian@gmail.com>
2024-02-11 19:56:46 +0200

    Fixed recommended articles on desktop not stretching properly

--- src/components/cards/LargeArticleCard.tsx
+++ src/components/cards/LargeArticleCard.tsx
@@ -1,7 +1,6 @@
 import { Article } from '@/lib/models';
 import Timestamp from '@/components/Timestamp';
 import OpenLinkButton from '@/components/buttons/OpenLinkButton';
-import Link from 'next/link';
 
 const LargeArticleCard = ({
 	article,
@@ -13,17 +12,15 @@ const LargeArticleCard = ({
 	const { image, title, createdAt, id } = article;
 
 	return (
-		<Link href={`/article/${id}`}>
-			<div className={`relative h-full ${className}`}>
-				<img
-					className='h-full object-cover cursor-pointer rounded-[3rem]'
-					src={image}
-					alt={title}
-				/>
-				<Timestamp className='absolute right-8 bottom-8' time={createdAt} />
-				<OpenLinkButton className='absolute right-8 top-8' />
-			</div>
-		</Link>
+		<div className={`grow flex relative h-full ${className}`}>
+			<img
+				className='h-full object-cover cursor-pointer rounded-[3rem] w-full'
+				src={image}
+				alt={title}
+			/>
+			<Timestamp className='absolute right-8 bottom-8' time={createdAt} />
+			<OpenLinkButton className='absolute right-8 top-8' />
+		</div>
 	);
 };
 
